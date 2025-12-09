6dd7d7156f7e0b5b39dc5e54774612529bfba77d
mspitsyn <mgraf@yandex.ru>
2025-12-09 15:04:36 +0300

    task1.1-3

--- 15.1/terraform/locals.tf
+++
@@ -1,9 +0,0 @@
-##locals.tf
-
-locals {
-  ssh-keys = file("~/.ssh/id_rsa.pub")
-  ssh-private-keys = file("~/.ssh/id_rsa")
-  cloud_id = "b1gchhnthvn0qe3v2rfb"
-  folder_id = "b1gh2n9lu2pm397sqa8m"
-  token = "y0__xDamLsEGMHdEyDr0OLjE-MJXOiYgG0lqcK8r0R4pjW8-Rh4"
-}
--- 15.1/terraform/main.tf
+++
@@ -1,5 +0,0 @@
-# main.tf
-
-resource "yandex_vpc_network" "network" {
-  name = var.vpc_network_name
-}
